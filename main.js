2ca0db8e5f60175c46b443e1873c495419c5ca91
Mohamed Gamal ElKelany <87882904+MoKelany1@users.noreply.github.com>
2022-08-20 00:50:19 +0200

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let upBtn = document.querySelector(".top-button");
-document.querySelector("body").onscroll = function() {
-    let Offset = pageYOffset;
-    if (Offset > 300) {
-        upBtn.style.visibility = "visible";
-    } else {
-        upBtn.style.visibility = "hidden";
-    }
-}
